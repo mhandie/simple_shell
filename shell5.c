b69bf3aec48d15239c9eb9cd8b49a5d04eb9b7a9
mhandie <anokpara@yahoo.com>
2023-06-28 08:09:56 -0700

    shell4

---
+++ shell5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char **environ;
+
+void print_environment(void)
+{
+char **env = environ;
+while (*env != NULL)
+{
+printf("%s\n", *env);
+env++;
+}
+}
+int main(void)
+{
+print_environment();
+return (0);
+}
