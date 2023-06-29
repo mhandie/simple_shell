c57191dd182a2ff0417a189f1f6b377c8100b1e0
FaizaAdamu <faizaadamu46@gmail.com>
2023-06-29 16:57:13 -0700

    shell

---
+++ shell1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "shell.h"
+/**
+* fay_main - Entry point
+* Description: shows numbers from 0 to 20.
+* Return: Always 0
+*/
+
+int fay_main(void)
+{
+int j;
+
+for (j = 0;  j < 20; j++)
+{
+printf("%d", j);
+}
+return (0);
+}
